e9d031240435da0a83a28b69c8f81fc4d9d5bb7b
CrisDeveloperRepo1 <44676837+CrisDeveloperRepo1@users.noreply.github.com>
2024-10-13 12:19:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function toggleText() {
 }
 
 // Llama a la función cada 3 segundos
-setInterval(toggleText, 5000); // Tiempo total entre apariciones
+setInterval(toggleText, 3000); // Tiempo total entre apariciones
 
 // Mostrar contenido de Introducción con efecto
 setTimeout(() => {
